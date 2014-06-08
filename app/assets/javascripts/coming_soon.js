be8bb12995c90c7d39761c385ea644e974edee13
dredupuika <andrievs.niedra@inbox.lv>
2014-06-08 12:03:25 +0300

    range selection

--- app/assets/javascripts/coming_soon.js
+++ app/assets/javascripts/coming_soon.js
@@ -2,7 +2,7 @@ jQuery(function()
 {
 	var container = jQuery('.container');
 	
-	var copy_buttons = continer.find('.share .button');
+	var copy_buttons = container.find('.share .button');
 	
 	if (container.is('.controller-coming_soon'))
 	{
@@ -10,7 +10,14 @@ jQuery(function()
 		{
 			var target = jQuery(this);
 			
-			
+			var doc = document;
+			var text = target.siblings('.copy_field')[0]; 
+	
+			var selection = window.getSelection();            
+			var range = doc.createRange();
+			range.selectNodeContents(text);
+			selection.removeAllRanges();
+			selection.addRange(range);
 		});
 	}
 });
