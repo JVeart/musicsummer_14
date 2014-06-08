19cfb4a673b166d0ec0ea50840aec836db7e58de
dredupuika <andrievs.niedra@inbox.lv>
2014-06-08 04:00:24 +0300

    most of the layout

---
+++ app/assets/javascripts/coming_soon.js
@@ -0,0 +1,16 @@
+jQuery(function()
+{
+	var container = jQuery('.container');
+	
+	var copy_buttons = continer.find('.share .button');
+	
+	if (container.is('.controller-coming_soon'))
+	{
+		copy_buttons.click(function()
+		{
+			var target = jQuery(this);
+			
+			
+		});
+	}
+});
