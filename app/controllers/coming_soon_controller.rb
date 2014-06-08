dc8b1945edf1e82e3ba9f8346b95b138ab5d077a
dredupuika <andrievs.niedra@inbox.lv>
2014-06-08 17:49:51 +0300

    calendar fix

--- app/controllers/coming_soon_controller.rb
+++ app/controllers/coming_soon_controller.rb
@@ -7,7 +7,7 @@ class ComingSoonController < ApplicationController
     @festivals.each do |festival|
       month = festival.start_date.month < 6 ? 6 : festival.start_date.month
       
-      if festival.end_date > Date.today
+      if festival.end_date >= Date.today
         unless @upcoming.has_key?(month)
           @upcoming[month] = []
         end
