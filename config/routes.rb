613f2ab1d7dee49101e5d97fdd38e2f6cec173a5
dredupuika <andrievs.niedra@inbox.lv>
2014-07-01 19:52:52 +0300

    deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root 'coming_soon#index'
+  root 'landing#index'
   
   resources :votes, :only => [ :new, :create]
   resources :artists, :only => [ :show ] do
